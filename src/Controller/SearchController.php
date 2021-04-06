3f0b4136d554ff5cbcbccd717c51e86d9b5326f5
Daniil Zavilohin <denik6740@gmail.com>
2021-04-06 14:24:44 +0300

    Создан контроллер для тестирования ElasticSearch

---
+++ src/Controller/SearchController.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Service\ElasticSearch\Entities\AnimeIndexer;
+use Elasticsearch\ClientBuilder;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SearchController extends AbstractController
+{
+    /**
+     * @Route("/search/")
+     * @param RequestStack $requestStack
+     * @param LoggerInterface $logger
+     */
+    public function search(RequestStack $requestStack, LoggerInterface $logger)
+    {
+        $client = (new ClientBuilder())->build();
+        $searchQuery = $requestStack->getCurrentRequest()->get('name');
+        $animeIndexer = new AnimeIndexer($client, $logger);
+
+        dd($animeIndexer->search(['name' => $searchQuery]));
+
+        return $this->render('home.html.twig');
+    }
+
+}
