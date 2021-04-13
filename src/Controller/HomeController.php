f9f3c511ce411308ac06c50f0374d6bfe3350b91
Daniil Zavilohin <denik6740@gmail.com>
2021-04-13 14:44:02 +0300

    Интеграция блока новинок на главную страницу

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,9 @@
 namespace App\Controller;
 
 
+use App\Entity\Anime;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ObjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,9 +16,14 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="index")
      */
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
-        return $this->render('home.html.twig');
+        $arRenderer['recommendations'] = $this->getRecommendations($entityManager->getRepository(Anime::class));
+        return $this->render('home.html.twig', $arRenderer);
     }
 
+    private function getRecommendations(ObjectRepository $animeRepository): array
+    {
+        return $animeRepository->findBy([], ['createdAt' => 'DESC'], 4);
+    }
 }
