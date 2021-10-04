71c72aef3c24ada66c7efff3fdc09eba1170396b
Daniil Zavilohin <denik6740@gmail.com>
2021-10-04 23:16:04 +0300

    Новая версия админ панели

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -8,7 +8,6 @@ use App\Entity\Screenshot;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
-use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,9 +18,7 @@ class DashboardController extends AbstractDashboardController
      */
     public function index(): Response
     {
-        $routeBuilder = $this->get(AdminUrlGenerator::class);
-
-        return $this->redirect($routeBuilder->setController(AnimeCrudController::class)->generateUrl());
+        return parent::index();
     }
 
     public function configureDashboard(): Dashboard
@@ -32,8 +29,6 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linktoDashboard('Сериалы', 'fa fa-home');
-        yield MenuItem::linkToCrud('Категории', 'fa fa-book', Categories::class);
-        yield MenuItem::linkToCrud('Кадры из сериалов', 'fa fa-picture-o', Screenshot::class);
+        yield MenuItem::linktoDashboard('Главная', 'fa fa-home');
     }
 }
