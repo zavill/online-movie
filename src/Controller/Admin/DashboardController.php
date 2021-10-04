e6a2f22d17fdd2fd746be54a4a41283b5f83d9ee
Daniil Zavilohin <denik6740@gmail.com>
2021-10-04 23:54:36 +0300

    Изменены доступы к админ панели

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -8,13 +8,9 @@ use App\Entity\Screenshot;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @IsGranted("ROLE_ADMIN")
- */
 class DashboardController extends AbstractDashboardController
 {
     /**
